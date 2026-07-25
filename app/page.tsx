7d6db46c1a1832d74712e51930a138f2eb4d6965
phnx119 <lucas.leon.schmitt@gmail.com>
2026-07-25 23:08:09 +0200

    mui

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@mui/material";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <p>maus</p>
+      <Button variant="outlined">test</Button>
     </div>
   );
 }
