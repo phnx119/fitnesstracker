6f8c1821d4bce2ecff25a75a81921d50e03335e7
phnx119 <lucas.leon.schmitt@gmail.com>
2026-07-26 21:55:14 +0200

    test

--- app/(tabs)/training/page.tsx
+++ app/(tabs)/training/page.tsx
@@ -1,20 +1,8 @@
 // app/(tabs)/exercise/page.tsx
 'use client';
 
-import { Box, Button, Stack } from '@mui/material';
+import { Stack } from '@mui/material';
 
 export default function ExercisePage() {
-    return (
-        <Stack sx={{ flex: 1, gap: 1, m: 1 }}>
-            <Box sx={{ flex: 1, bgcolor: 'red' }} />
-            <Button>maus</Button>
-            <Box sx={{ flex: 1, bgcolor: 'green' }} />
-
-            <Stack direction="row" sx={{ bgcolor: 'blue' }}>
-                <Button variant="contained">maus2</Button>
-                <Box sx={{ flex: 1 }} />
-                <Button variant="contained">maus2</Button>
-            </Stack>
-        </Stack>
-    );
+    return <Stack sx={{ flex: 1, gap: 1, m: 1 }}></Stack>;
 }
