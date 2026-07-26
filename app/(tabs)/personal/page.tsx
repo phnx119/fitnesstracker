998a35c0fe61e78ed2edadf10304101b2c2cfbb9
PDittert <paul.dittert@a-w.com>
2026-07-26 21:32:56 +0200

    Grundstruktur für Personal

--- app/(tabs)/personal/page.tsx
+++ app/(tabs)/personal/page.tsx
@@ -1,17 +1,17 @@
 // app/(tabs)/exercise/page.tsx
 'use client';
 
-import { Stack, Button, Typography } from "@mui/material";
+import { Stack, Typography, Box } from "@mui/material";
 
 export default function ExercisePage() {
   return (
-    <Stack sx={{flex: 1}}>
-      <Typography>
-        Personal
-      </Typography>
-      <Button variant="contained" color="primary">
-        maus
-      </Button>
+    <Stack sx={{flex: 1, gap: 1, m: 1, bgcolor: "red"}} >
+      <Stack direction="row" sx={{bgcolor: "blue"}}>
+        <Box sx={{flex: 1}}/>
+        <Typography>Overview</Typography>
+        <Box sx={{flex: 1}}/>
+        
+      </Stack>
     </Stack>
   );
 }
