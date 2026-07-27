8c24a53077561bea29eb362419456d51c31a2ff3
phnx119 <lucas.leon.schmitt@gmail.com>
2026-07-27 20:25:42 +0200

    loading

--- app/(tabs)/loading.tsx
+++ app/(tabs)/loading.tsx
@@ -1,8 +1,9 @@
-import { Button, Stack } from '@mui/material';
+import { Button, Stack, Typography } from '@mui/material';
 
 export default function loading() {
     return (
         <Stack sx={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Typography variant="h1">Loading</Typography>
             <Button loading variant="text" disabled />
         </Stack>
     );
