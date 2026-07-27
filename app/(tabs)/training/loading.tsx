5528259137a7d63956cef786b0cb34704895c197
phnx119 <lucas.leon.schmitt@gmail.com>
2026-07-27 20:16:56 +0200

    loading test

---
+++ app/(tabs)/training/loading.tsx
@@ -0,0 +1,11 @@
+import { Stack, Typography } from '@mui/material';
+
+export default function loading() {
+    return (
+        <Stack sx={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Typography>maus</Typography>
+            <Typography>maus</Typography>
+            <Typography>maus</Typography>
+        </Stack>
+    );
+}
