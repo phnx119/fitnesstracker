02ac5d3f6d717091bdaf75e23233229609f71381
Plascha <paulsimondittert@gmail.com>
2026-07-28 08:43:52 +0200

    Personal | Stack growth test for phone

--- app/(tabs)/personal/page.tsx
+++ app/(tabs)/personal/page.tsx
@@ -4,13 +4,13 @@ import { Box, Card, CardContent, Stack, Typography } from '@mui/material';
 
 export default function ExercisePage() {
     return (
-        <Stack sx={{ flex: 1, gap: 1, m: 1 }}>
+        <Stack sx={{ flex: 1, gap: 1, m: 1, bgcolor: 'blue' }}>
             <Stack direction="row" sx={{}}>
                 <Box sx={{ flex: 1 }} />
                 <Typography>Overview</Typography>
                 <Box sx={{ flex: 1 }} />
             </Stack>
-            <Stack sx={{ flex: 1, gap: 1, m: 1 }}>
+            <Stack sx={{ gap: 1, m: 1, overflowY: 'auto', maxHeight: 500 }}>
                 <Card variant="outlined">
                     <CardContent>
                         <Typography variant="h6">Bodyheight</Typography>
@@ -19,31 +19,43 @@ export default function ExercisePage() {
                 </Card>
                 <Card variant="outlined">
                     <CardContent>
-                        <Typography variant="h6">Bodyweight</Typography>
+                        <Typography variant="h6">Bodyweight1</Typography>
                         <Typography variant="body1">Emptycheck2</Typography>
                     </CardContent>
                 </Card>
                 <Card variant="outlined">
                     <CardContent>
-                        <Typography variant="h6">Bodyweight</Typography>
+                        <Typography variant="h6">Bodyweight2</Typography>
                         <Typography variant="body1">Emptycheck2</Typography>
                     </CardContent>
                 </Card>
                 <Card variant="outlined">
                     <CardContent>
-                        <Typography variant="h6">Bodyweight</Typography>
+                        <Typography variant="h6">Bodyweight3</Typography>
                         <Typography variant="body1">Emptycheck2</Typography>
                     </CardContent>
                 </Card>
                 <Card variant="outlined">
                     <CardContent>
-                        <Typography variant="h6">Bodyweight</Typography>
+                        <Typography variant="h6">Bodyweight4</Typography>
                         <Typography variant="body1">Emptycheck2</Typography>
                     </CardContent>
                 </Card>
                 <Card variant="outlined">
                     <CardContent>
-                        <Typography variant="h6">Bodyweight</Typography>
+                        <Typography variant="h6">Bodyweight5</Typography>
+                        <Typography variant="body1">Emptycheck2</Typography>
+                    </CardContent>
+                </Card>
+                <Card variant="outlined">
+                    <CardContent>
+                        <Typography variant="h6">Bodyweight6</Typography>
+                        <Typography variant="body1">Emptycheck2</Typography>
+                    </CardContent>
+                </Card>
+                <Card variant="outlined">
+                    <CardContent>
+                        <Typography variant="h6">Bodyweight7</Typography>
                         <Typography variant="body1">Emptycheck2</Typography>
                     </CardContent>
                 </Card>
