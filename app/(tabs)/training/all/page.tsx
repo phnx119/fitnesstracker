080cba19c10b5833772c3f47883800d725011105
phnx119 <lucas.leon.schmitt@gmail.com>
2026-07-31 22:11:33 +0200

    numberfield  kann jetzt wirklich zwirbeln

--- app/(tabs)/training/all/page.tsx
+++ app/(tabs)/training/all/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import NumberField from '@/components/NumberField';
 import { Button } from '@mui/material';
 import PlanContainer from '../PlanContainer';
 
@@ -7,6 +8,7 @@ export default function allMachines() {
     return (
         <PlanContainer title="All" headerButtons={null}>
             <Button>maus?</Button>
+            <NumberField label="mausigemausmaus" showButtons />
         </PlanContainer>
     );
 }
