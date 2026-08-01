a3314f521b9e39b0c64687749c71a1bc983fc2a2
phnx119 <lucas.leon.schmitt@gmail.com>
2026-08-02 00:54:41 +0200

    eslint unused fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,7 +9,7 @@ const eslintConfig = defineConfig([
     {
         rules: {
             '@typescript-eslint/no-unused-vars': [
-                'error',
+                'warn',
                 {
                     argsIgnorePattern: '^_',
                     varsIgnorePattern: '^_',
