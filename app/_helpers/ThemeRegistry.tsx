964da0dc6d14d7334f5e7f8158956e5f726856de
phnx119 <lucas.leon.schmitt@gmail.com>
2026-08-03 19:47:13 +0200

    better emotioncache

--- app/_helpers/ThemeRegistry.tsx
+++ app/_helpers/ThemeRegistry.tsx
@@ -8,38 +8,46 @@ import { ThemeProvider } from '@mui/material/styles';
 import { useServerInsertedHTML } from 'next/navigation';
 import * as React from 'react';
 
+function createEmotionCache() {
+    const cache = createCache({ key: 'mui', prepend: true });
+    cache.compat = true;
+
+    const prevInsert = cache.insert;
+    let inserted: string[] = [];
+
+    cache.insert = (...args) => {
+        const serialized = args[1];
+        if (cache.inserted[serialized.name] === undefined) {
+            inserted.push(serialized.name);
+        }
+        return prevInsert(...args);
+    };
+
+    const flush = () => {
+        const prevInserted = inserted;
+        inserted = [];
+        return prevInserted;
+    };
+
+    return { cache, flush };
+}
+
 export default function ThemeRegistry({
     children,
 }: {
     children: React.ReactNode;
 }) {
-    const [{ cache, flush }] = React.useState(() => {
-        const cache = createCache({ key: 'mui' });
-        cache.compat = true;
-        const prevInsert = cache.insert;
-        let inserted: string[] = [];
-        cache.insert = (...args) => {
-            const serialized = args[1];
-            if (cache.inserted[serialized.name] === undefined) {
-                inserted.push(serialized.name);
-            }
-            return prevInsert(...args);
-        };
-        const flush = () => {
-            const prevInserted = inserted;
-            inserted = [];
-            return prevInserted;
-        };
-        return { cache, flush };
-    });
+    const [{ cache, flush }] = React.useState(createEmotionCache);
 
     useServerInsertedHTML(() => {
         const names = flush();
         if (names.length === 0) return null;
+
         let styles = '';
         for (const name of names) {
             styles += cache.inserted[name];
         }
+
         return (
             <style
                 key={cache.key}
