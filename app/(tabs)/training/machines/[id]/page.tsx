c513582703df455c39c0a92ea256c319b0166e34
phnx119 <lucas.leon.schmitt@gmail.com>
2026-08-04 23:40:11 +0200

    editing machines

--- app/(tabs)/training/machines/[id]/page.tsx
+++ app/(tabs)/training/machines/[id]/page.tsx
@@ -1,18 +1,42 @@
 'use client';
 
 import { dbInstance } from '@/database/db';
-import { Button } from '@mui/material';
+import { Settings } from '@mui/icons-material';
+import { Button, Dialog, IconButton } from '@mui/material';
 import { useLiveQuery } from 'dexie-react-hooks';
 import { useParams } from 'next/navigation';
+import { useState } from 'react';
 import TrainingContainer from '../../TrainingContainer';
+import EditMachineDialog from '../EditMachineDialog';
 
 export default function MachinePage() {
     const { id: idString } = useParams<{ id: string }>();
     const machineId = Number(idString);
     const machine = useLiveQuery(() => dbInstance.Machine.get(machineId));
+
+    const [showEditDialog, setShowEditDialog] = useState(false);
+
     return machine ? (
-        <TrainingContainer title={machine.name}>
+        <TrainingContainer
+            title={machine.name}
+            headerButtons={
+                <IconButton onClick={() => setShowEditDialog(true)}>
+                    <Settings />
+                </IconButton>
+            }
+        >
             <Button>maus</Button>
+
+            <Dialog open={showEditDialog} onClose={closeEditDialog}>
+                <EditMachineDialog
+                    onClose={closeEditDialog}
+                    machine={machine}
+                />
+            </Dialog>
         </TrainingContainer>
     ) : null;
+
+    function closeEditDialog() {
+        setShowEditDialog(false);
+    }
 }
