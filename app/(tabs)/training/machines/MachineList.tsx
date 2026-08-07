4ccbaa7fda4084eaf2f2008038ce863b909d332b
phnx119 <lucas.leon.schmitt@gmail.com>
2026-08-07 21:01:58 +0200

    fixed imagelistitem size

--- app/(tabs)/training/machines/MachineList.tsx
+++ app/(tabs)/training/machines/MachineList.tsx
@@ -18,31 +18,29 @@ export default function MachineList({
     selectedIds?: number[];
 }) {
     return (
-        <Stack sx={{ flex: 1, overflow: 'auto' }}>
-            <ImageList cols={2} gap={8} sx={{ overflow: 'auto', flex: 1 }}>
-                {machines.map((item) => (
-                    <ImageListItem key={item.id} onClick={() => onClick(item)}>
-                        <BlobImage blob={item.imageBlob} />
+        <ImageList cols={2} gap={8} sx={{ overflow: 'auto' }}>
+            {machines.map((item) => (
+                <ImageListItem key={item.id} onClick={() => onClick(item)}>
+                    <BlobImage blob={item.imageBlob} />
 
-                        {selectedIds.some((selId) => selId === item.id) && (
-                            <Stack
-                                sx={{
-                                    position: 'absolute',
-                                    justifyContent: 'center',
-                                    alignItems: 'center',
-                                    height: '100%',
-                                    width: '100%',
-                                    bgcolor: '#000000A0',
-                                }}
-                            >
-                                <CheckBox sx={{ fontSize: 40 }} />
-                            </Stack>
-                        )}
+                    {selectedIds.some((selId) => selId === item.id) && (
+                        <Stack
+                            sx={{
+                                position: 'absolute',
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                                height: '100%',
+                                width: '100%',
+                                bgcolor: '#000000A0',
+                            }}
+                        >
+                            <CheckBox sx={{ fontSize: 40 }} />
+                        </Stack>
+                    )}
 
-                        <ImageListItemBar title={item.name} />
-                    </ImageListItem>
-                ))}
-            </ImageList>
-        </Stack>
+                    <ImageListItemBar title={item.name} />
+                </ImageListItem>
+            ))}
+        </ImageList>
     );
 }
