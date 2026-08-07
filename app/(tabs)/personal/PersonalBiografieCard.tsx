e6e033dfddf1d01881caa8f2b82d91d62aa966e7
phnx119 <lucas.leon.schmitt@gmail.com>
2026-08-07 18:58:02 +0200

    hinweis

--- app/(tabs)/personal/PersonalBiografieCard.tsx
+++ app/(tabs)/personal/PersonalBiografieCard.tsx
@@ -14,6 +14,15 @@ export default function PersonalBiografieCard({
     attName,
     bioValue,
 }: Props): JSX.Element {
+    //mausmausmausmausmausmausmausmausmausmausmausmausmausmausmausmausmausmausmausmausmausmausmausmausmausmausmausmausmausmausmausmausmausmausmausmausmausmausmausmausmausmaus
+    // Hinweis zum CardContent:
+    //   Dieser ist dafür gedacht, ein sinnvolles und einheitliches padding in einer card zu bekommen, wenn man noch CardHeader und CardActions benutzt.
+    //   Der ist nicht dafür ausgelegt, den alleine zu benutzen
+    //   Wenn du dir das padding über und unter den Numberfields anschaust, wirst du sehen, dass die nicht gleich sind...
+    //   Falls du also den Inhalt deiner Cards zentriert haben möchtest, solltest du einfach einen normalen Stack nehmen mit idk padding: 2 oder sowas
+    //
+    //   (Warum man das padding da so eingestellt hat, verstehe ich selbst nicht... ich hasse den CardContent)
+    //mausmausmausmausmausmausmausmausmausmausmausmausmausmausmausmausmausmausmausmausmausmausmausmausmausmausmausmausmausmausmausmausmausmausmausmausmausmausmausmausmausmaus
     return (
         <Card variant="outlined">
             <CardContent>
