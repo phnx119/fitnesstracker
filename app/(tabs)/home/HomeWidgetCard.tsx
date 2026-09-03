7ca1e610ee0fcf501c3b5ade467a9d52f4554c15
phnx119 <lucas.leon.schmitt@gmail.com>
2026-09-04 01:52:50 +0200

    home widget card

---
+++ app/(tabs)/home/HomeWidgetCard.tsx
@@ -0,0 +1,21 @@
+import { Card, Stack, Typography } from '@mui/material';
+import { PropsWithChildren } from 'react';
+
+export default function HomeWidgetCard({
+    flex = 1,
+    title = undefined,
+    children,
+}: PropsWithChildren<{ flex?: number; title?: string | undefined }>) {
+    return (
+        <Stack sx={{ flex: flex }}>
+            <Card sx={{ flex: flex, p: 1 }}>
+                {title && (
+                    <Stack sx={{ justifyContent: 'center', ml: 2, mt: 1 }}>
+                        <Typography color="textSecondary">{title}</Typography>
+                    </Stack>
+                )}
+                {children}
+            </Card>
+        </Stack>
+    );
+}
--- app/(tabs)/home/page.tsx
+++ app/(tabs)/home/page.tsx
@@ -1,11 +1,13 @@
 import SideNav from '@/app/_helpers/SideNav';
-import { Stack, TextField } from '@mui/material';
+import { Stack } from '@mui/material';
+import HomeWidgetCard from './HomeWidgetCard';
 
 export default function Home() {
     return (
         <Stack sx={{ flex: 1, overflow: 'auto' }} direction="row">
-            <Stack sx={{ flex: 1, overflow: 'auto' }}>
-                <TextField></TextField>
+            <Stack sx={{ flex: 1, overflow: 'auto', p: 1 }}>
+                <HomeWidgetCard title="Last used Plans"></HomeWidgetCard>
+                <Stack sx={{ flex: 4 }}></Stack>
             </Stack>
             <SideNav />
         </Stack>
