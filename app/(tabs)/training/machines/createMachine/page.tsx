23a5366593a2ea8e3b82dee6fcdfc72a369ae46d
phnx119 <lucas.leon.schmitt@gmail.com>
2026-09-03 13:02:14 +0200

    create machine improved

---
+++ app/(tabs)/training/machines/createMachine/page.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import Header from '@/components/Header';
+import { dbInstance } from '@/database/db';
+import { Button, Stack, TextField } from '@mui/material';
+import { useState } from 'react';
+
+export default function CreateMachine() {
+    const [machineName, setMachineName] = useState('');
+
+    return (
+        <>
+            <Header title="Add Machine" showClose>
+                <Button onClick={createMachine}>Save</Button>
+            </Header>
+
+            <Stack sx={{ gap: 1, flex: 1, overflow: 'auto', p: 2 }}>
+                <TextField
+                    label="Name"
+                    value={machineName}
+                    onChange={(e) => setMachineName(e.target.value)}
+                />
+            </Stack>
+        </>
+    );
+
+    function createMachine() {
+        dbInstance.Machine.add({
+            name: machineName,
+            imageBlob: undefined,
+        });
+    }
+}
--- app/(tabs)/training/machines/page.tsx
+++ app/(tabs)/training/machines/page.tsx
@@ -1,19 +1,16 @@
 'use client';
 
 import { dbInstance, Row } from '@/database/db';
-import { Button, Dialog, Stack } from '@mui/material';
+import { Button, Stack } from '@mui/material';
 import { useLiveQuery } from 'dexie-react-hooks';
 import { usePathname, useRouter } from 'next/navigation';
-import { useState } from 'react';
 import TrainingContainer from '../TrainingContainer';
-import AddMachineDialog from './AddMachineDialog';
 import MachineList from './MachineList';
 
 export default function AllMachinesPage() {
     const pathName = usePathname();
     const router = useRouter();
     const allMachines = useLiveQuery(() => dbInstance.Machine.toArray()) ?? [];
-    const [showAddDialog, setShowAddDialog] = useState(false);
     return (
         <TrainingContainer title="All" headerButtons={null}>
             <Stack sx={{ gap: 1, overflow: 'auto', flex: 1 }}>
@@ -22,20 +19,16 @@ export default function AllMachinesPage() {
                     onClick={handleMachineClick}
                 />
 
-                <Button onClick={() => setShowAddDialog(true)}>add</Button>
+                <Button
+                    onClick={() => router.push(`${pathName}/createMachine`)}
+                >
+                    Create Machine
+                </Button>
             </Stack>
-
-            <Dialog open={showAddDialog} onClose={closeAddDialog} fullScreen>
-                <AddMachineDialog onClose={closeAddDialog} />
-            </Dialog>
         </TrainingContainer>
     );
 
     function handleMachineClick(machine: Row<'Machine'>) {
         router.push(`${pathName}/${machine.id}`);
-    }
-
-    function closeAddDialog() {
-        setShowAddDialog(false);
     }
 }
