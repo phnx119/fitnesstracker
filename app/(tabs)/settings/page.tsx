bff2bfcfb3550709b7fa25a4f385e5ffd5d23728
phnx119 <lucas.leon.schmitt@gmail.com>
2026-09-04 15:45:48 +0200

    import fix

--- app/(tabs)/settings/page.tsx
+++ app/(tabs)/settings/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { FIXED_NAV_ITEMS } from '@/app/_helpers/BottomNav';
+import { FIXED_NAV_ITEMS } from '@/app/_helpers/SideNav';
 import Header from '@/components/Header';
 import TabContentStack from '@/components/TabContentStack';
 import { dbInstance } from '@/database/db';
