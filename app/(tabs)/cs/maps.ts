8aa11ae37983d0643f39febfbc802fb84ee7b57f
phnx119 <lucas.leon.schmitt@gmail.com>
2026-09-04 22:00:38 +0200

    pipeline fix

---
+++ app/(tabs)/cs/maps.ts
@@ -0,0 +1,44 @@
+export const CS_MAPS = [
+    {
+        id: 1,
+        name: 'ancient',
+        path: 'ancient',
+        image: null,
+    },
+    {
+        id: 2,
+        name: 'anubis',
+        path: 'anubis',
+        image: null,
+    },
+    {
+        id: 3,
+        name: 'cache',
+        path: 'cache',
+        image: null,
+    },
+    {
+        id: 4,
+        name: 'dust',
+        path: 'dust',
+        image: null,
+    },
+    {
+        id: 5,
+        name: 'inferno',
+        path: 'inferno',
+        image: null,
+    },
+    {
+        id: 6,
+        name: 'mirage',
+        path: 'mirage',
+        image: null,
+    },
+    {
+        id: 7,
+        name: 'nuke',
+        path: 'nuke',
+        image: null,
+    },
+];
--- app/(tabs)/cs/page.tsx
+++ app/(tabs)/cs/page.tsx
@@ -1,50 +1,5 @@
 import { redirect } from 'next/navigation';
 
 export default function Cs() {
-    redirect('cs/mirage/t');
+    redirect('/cs/mirage/t');
 }
-
-export const MAPS = [
-    {
-        id: 1,
-        name: 'ancient',
-        path: 'ancient',
-        image: null,
-    },
-    {
-        id: 2,
-        name: 'anubis',
-        path: 'anubis',
-        image: null,
-    },
-    {
-        id: 3,
-        name: 'cache',
-        path: 'cache',
-        image: null,
-    },
-    {
-        id: 4,
-        name: 'dust',
-        path: 'dust',
-        image: null,
-    },
-    {
-        id: 5,
-        name: 'inferno',
-        path: 'inferno',
-        image: null,
-    },
-    {
-        id: 6,
-        name: 'mirage',
-        path: 'mirage',
-        image: null,
-    },
-    {
-        id: 7,
-        name: 'nuke',
-        path: 'nuke',
-        image: null,
-    },
-];
