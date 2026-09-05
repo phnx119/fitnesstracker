38dc62bfcd35fd5317bed167850a389bd5179efe
phnx119 <lucas.leon.schmitt@gmail.com>
2026-09-05 20:50:16 +0200

    textoverlay for csimage

--- app/(tabs)/cs/CsImage.tsx
+++ app/(tabs)/cs/CsImage.tsx
@@ -1,5 +1,29 @@
+import { Stack, Typography } from '@mui/material';
 import { StaticImport } from 'next/dist/shared/lib/get-img-props';
 import Image from 'next/image';
-export default function CsImage({ src }: { src: string | StaticImport }) {
-    return <Image alt="image" src={src} fill priority />;
+export default function CsImage({
+    src,
+    text = '',
+}: {
+    src: string | StaticImport;
+    text?: string;
+}) {
+    return (
+        <Stack>
+            {text !== '' && (
+                <Typography
+                    sx={{
+                        position: 'absolute',
+                        top: 2,
+                        left: 2,
+                        backgroundColor: 'black',
+                        zIndex: 10,
+                    }}
+                >
+                    test
+                </Typography>
+            )}
+            <Image alt="image" src={src} fill priority />;
+        </Stack>
+    );
 }
