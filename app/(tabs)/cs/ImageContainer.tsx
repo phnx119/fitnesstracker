eda94aa53a4902de5cf73a20caedd4c02be85aa9
phnx119 <lucas.leon.schmitt@gmail.com>
2026-09-06 19:22:27 +0200

    zoom dialog

--- app/(tabs)/cs/ImageContainer.tsx
+++ app/(tabs)/cs/ImageContainer.tsx
@@ -1,24 +1,41 @@
-import { Stack } from '@mui/material';
-import { PropsWithChildren } from 'react';
+import { Dialog, Stack } from '@mui/material';
+import { PropsWithChildren, useState } from 'react';
 
 export default function ImageContainer({
     children,
     width = '100%',
     height = '100%',
+    dialogAspectRatio = 1,
     direction = 'column',
 }: PropsWithChildren<{
     width?: string;
     height?: string;
+    dialogAspectRatio?: number;
     direction?: 'column' | 'row';
 }>) {
+    const [openDialog, setOpenDialog] = useState(false);
     return (
-        <Stack sx={{ flex: 1, maxHeight: height }}>
+        <>
             <Stack
-                sx={{ width: width, height: height, position: 'relative' }}
-                direction={direction}
+                sx={{ flex: 1, maxHeight: height }}
+                onClick={() => setOpenDialog(true)}
             >
-                {children}
+                <Stack
+                    sx={{ width: width, height: height, position: 'relative' }}
+                    direction={direction}
+                >
+                    {children}
+                </Stack>
             </Stack>
-        </Stack>
+            <Dialog open={openDialog} onClose={closeDialog}>
+                <Stack sx={{ p: 1, aspectRatio: dialogAspectRatio }}>
+                    {children}
+                </Stack>
+            </Dialog>
+        </>
     );
+
+    function closeDialog() {
+        setOpenDialog(false);
+    }
 }
