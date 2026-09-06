2675fa8dea5f07ccade8246565abdeb2b58ad96a
phnx119 <lucas.leon.schmitt@gmail.com>
2026-09-06 02:56:43 +0200

    input icons

--- app/(tabs)/cs/CsImage.tsx
+++ app/(tabs)/cs/CsImage.tsx
@@ -1,13 +1,27 @@
+import LMBIcon from '@/public/inputIcons/LMBIcon.webp';
+import LMBRMBIcon from '@/public/inputIcons/LMBRMBIcon.webp';
+import RMBIcon from '@/public/inputIcons/RMBIcon.webp';
+
 import { Stack, Typography } from '@mui/material';
+import { IconLetterW, IconTagsChevronUp } from '@tabler/icons-react/';
 import { StaticImport } from 'next/dist/shared/lib/get-img-props';
 import Image from 'next/image';
 export default function CsImage({
     src,
     text = '',
+    lmb = false,
+    rmb = false,
+    jump = false,
+    w = false,
 }: {
     src: string | StaticImport;
     text?: string;
+    lmb?: boolean;
+    rmb?: boolean;
+    jump?: boolean;
+    w?: boolean;
 }) {
+    const mouseIcon = lmb ? (rmb ? LMBRMBIcon : LMBIcon) : RMBIcon;
     return (
         <Stack>
             {text !== '' && (
@@ -24,6 +38,27 @@ export default function CsImage({
                 </Typography>
             )}
             <Image alt="image" src={src} fill priority />;
+            {(lmb || rmb || w || jump) && (
+                <Stack
+                    direction="row"
+                    sx={{
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        bottom: 0,
+                        position: 'absolute',
+                        bgcolor: 'black',
+                        height: 30,
+                    }}
+                >
+                    {(lmb || rmb) && (
+                        <Stack sx={{ mx: -2 }}>
+                            <Image alt="image" src={mouseIcon} height="30" />
+                        </Stack>
+                    )}
+                    {jump && <IconTagsChevronUp />}
+                    {w && <IconLetterW height="50" />}
+                </Stack>
+            )}
         </Stack>
     );
 }
