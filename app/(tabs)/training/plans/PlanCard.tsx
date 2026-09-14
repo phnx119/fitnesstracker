33c782bf046c81e97edb55f8193ab3d04ab1dd89
phnx119 <lucas.leon.schmitt@gmail.com>
2026-09-14 12:31:01 +0200

    added last used to plan list

--- app/(tabs)/training/plans/PlanCard.tsx
+++ app/(tabs)/training/plans/PlanCard.tsx
@@ -2,7 +2,7 @@
 
 import { BlobImage } from '@/components/BlobImage';
 import { Row } from '@/database/db';
-import { Card, Stack, Typography } from '@mui/material';
+import { Box, Card, Stack, Typography } from '@mui/material';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
@@ -24,6 +24,21 @@ export default function PlanCard({ plan }: { plan: Row<'WorkoutPlan'> }) {
                         <BlobImage blob={plan.imageBlob} />
                     </Stack>
                     <Typography>{plan.name}</Typography>
+
+                    <Box sx={{ flex: 1 }} />
+
+                    {plan.lastUsed && (
+                        <Typography>
+                            {new Date(plan.lastUsed).toLocaleDateString(
+                                'de-DE',
+                                {
+                                    day: '2-digit',
+                                    month: '2-digit',
+                                    year: '2-digit',
+                                },
+                            )}
+                        </Typography>
+                    )}
                 </Stack>
             </Card>
         </Link>
