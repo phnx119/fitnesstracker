eef2b2844259dceadf3f432f5dc246f74b2df29f
phnx119 <lucas.leon.schmitt@gmail.com>
2026-09-14 12:34:24 +0200

    last used links to actual plans

--- app/(tabs)/home/LastUsedPlans.tsx
+++ app/(tabs)/home/LastUsedPlans.tsx
@@ -3,6 +3,7 @@
 import { dbInstance } from '@/database/db';
 import { Box, Card, Stack, Typography } from '@mui/material';
 import { useLiveQuery } from 'dexie-react-hooks';
+import Link from 'next/link';
 import HomeWidgetCard from './HomeWidgetCard';
 
 export default function LastUsedPlans() {
@@ -16,26 +17,27 @@ export default function LastUsedPlans() {
     return (
         <HomeWidgetCard title="Last used Plans" flex={1}>
             {lastPlans?.map((plan) => (
-                <Stack key={plan.id}>
-                    <Card sx={{ p: 1, px: 2 }}>
-                        <Stack direction="row">
-                            <Typography>{plan.name}</Typography>
-                            <Box sx={{ flex: 1 }} />
-                            {plan.lastUsed && (
-                                <Typography>
-                                    {new Date(plan.lastUsed).toLocaleDateString(
-                                        'de-DE',
-                                        {
+                <Link href={`/training/plans/${plan.id}`} key={plan.id}>
+                    <Stack>
+                        <Card sx={{ p: 1, px: 2 }}>
+                            <Stack direction="row">
+                                <Typography>{plan.name}</Typography>
+                                <Box sx={{ flex: 1 }} />
+                                {plan.lastUsed && (
+                                    <Typography>
+                                        {new Date(
+                                            plan.lastUsed,
+                                        ).toLocaleDateString('de-DE', {
                                             day: '2-digit',
                                             month: '2-digit',
                                             year: '2-digit',
-                                        },
-                                    )}
-                                </Typography>
-                            )}
-                        </Stack>
-                    </Card>
-                </Stack>
+                                        })}
+                                    </Typography>
+                                )}
+                            </Stack>
+                        </Card>
+                    </Stack>
+                </Link>
             ))}
         </HomeWidgetCard>
     );
